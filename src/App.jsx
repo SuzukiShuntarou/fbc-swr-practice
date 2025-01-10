31057cafdff35ca0bb8c81a36dfa13a8356ff94b
SuzukiShuntarou <da8801593@gmail.com>
2025-01-10 10:33:10 +0900

    SWRでデータを取得する

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import useSWR from "swr";
 import "./App.css";
 
@@ -6,18 +5,12 @@ function App() {
   const url = "https://httpstat.us/200?sleep=2000";
   const headers = { Accept: "application/json" };
 
-  const [status, setStatus] = useState("");
-
-  const fetcher = (url) =>
-    fetch(url, { headers })
-      .then((res) => res.json())
-      .then((json) => setStatus(json.description));
-
-  const { error, isLoading } = useSWR(url, fetcher);
+  const fetcher = (url) => fetch(url, { headers }).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(url, fetcher);
 
   if (error) return <div>Failed to load.</div>;
   if (isLoading) return <div>loading...</div>;
-  return <>{status && <p>Status : {status}</p>}</>;
+  return <>{data && <p>Status : {data.description}</p>}</>;
 }
 
 export default App;
